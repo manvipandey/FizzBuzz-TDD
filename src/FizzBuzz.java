c958197e309c8f00bc1bf7a39d628ccbc61956cc
Manvi Pandey <manvi.muskan@gmail.com>
2020-10-29 21:09:44 +0530

    Testing multiples of three

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,13 @@ import org.junit.Test;
 public class FizzBuzz {
 
     public String action(int input) {
-        return String.valueOf(input);
+
+        if(input % 3 == 0) {
+            return "Fizz";
+        }
+        else {
+            return String.valueOf(input);
+        }
     }
 
 }
--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -7,8 +7,13 @@ public class FizzBuzzTest {
     public void test() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
+        // Print the number itself if it's not divisible by either of 3 or 5
         Assert.assertEquals("1", fizzBuzz.action(1));
         Assert.assertEquals("2", fizzBuzz.action(2));
+
+        // Print "Fizz" if the number is a multiple of three
+        Assert.assertEquals("Fizz", fizzBuzz.action(3));
+        Assert.assertEquals("Fizz", fizzBuzz.action(9));
 
     }
 
